b22e9ac2b57b8f914ac31217df8e5e466e9aa786
Alisson Ally <alissonbrg32@gmail.com>
2025-11-27 00:19:36 -0300

    refactor: implement pagination for chamadas in Dashboard component

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -21,6 +21,8 @@ function Dashboard() {
   const [lag, setLag] = useState(0);
   const [long, setLong] = useState(0);
 
+  const [chamadasTablePage, setChamadasTablePage] = useState(0);
+
   const handleCriarChamada = async () => {
     fetchLocation();
     const newChamada = await createChamada(lag, long);
@@ -69,29 +71,51 @@ function Dashboard() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {chamadas.map((item) => (
-              <TableRow
-                key={item._id}
-                onClick={() => {
-                  navigate("/chamada/" + item._id);
-                }}
-              >
-                <TableCell className="font-medium">{item.nome}</TableCell>
-                <TableCell>{item.ativa ? "Ativa" : "Desabilitada"}</TableCell>
-                <TableCell>
-                  {new Date(item.dataInicio).toLocaleDateString()}
-                </TableCell>
-                <TableCell>
-                  {new Date(item.dataFim).toLocaleDateString()}
-                </TableCell>
-                <TableCell>
-                  Lag: {item.lag} Long: {item.long}
-                </TableCell>
-                <TableCell>{item.toleranceMeters}m</TableCell>
-              </TableRow>
-            ))}
+            {chamadas
+              .filter(
+                (_, i) =>
+                  i + 1 > chamadasTablePage * 10 &&
+                  i + 1 < chamadasTablePage * 10 + 10
+              )
+              .map((item) => (
+                <TableRow
+                  key={item._id}
+                  onClick={() => {
+                    navigate("/chamada/" + item._id);
+                  }}
+                >
+                  <TableCell className="font-medium">{item.nome}</TableCell>
+                  <TableCell>{item.ativa ? "Ativa" : "Desabilitada"}</TableCell>
+                  <TableCell>
+                    {new Date(item.dataInicio).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>
+                    {new Date(item.dataFim).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>
+                    Lag: {item.lag} Long: {item.long}
+                  </TableCell>
+                  <TableCell>{item.toleranceMeters}m</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
+      </div>
+      <div className="flex gap-1">
+        {Array.from(
+          { length: Math.ceil(chamadas.length / 10) },
+          (_, i) => i + 1
+        ).map((val, i) => (
+          <Button
+            size={"sm"}
+            variant={chamadasTablePage == i ? "default" : "secondary"}
+            onClick={() => {
+              setChamadasTablePage(i);
+            }}
+          >
+            {val}
+          </Button>
+        ))}
       </div>
       <Button onClick={handleCriarChamada}>Criar Chamada</Button>
     </div>
